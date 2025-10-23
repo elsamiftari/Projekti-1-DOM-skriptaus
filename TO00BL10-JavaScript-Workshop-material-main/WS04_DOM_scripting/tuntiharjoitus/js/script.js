4bd1f9f8c328c444213faaea1d0559f889f94130
elsamiftari <elsa.miftari@student.laurea.fi>
2025-10-23 13:13:10 +0300

    Delete TO00BL10-JavaScript-Workshop-material-main directory

--- TO00BL10-JavaScript-Workshop-material-main/WS04_DOM_scripting/tuntiharjoitus/js/script.js
+++
@@ -1,5 +0,0 @@
-let d = new Date();
-alert("Today's date is " + d);
-
-let d2 = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d2 + "</h1>"
--- TO00BL10-JavaScript-Workshop-material-main/WS12_TS/TypeScript_WS-main/readme.md
+++
@@ -1,1 +0,0 @@
-Laurean TypeScript työpajamateriaali
